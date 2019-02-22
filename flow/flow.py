663fc472cdfeec61c00760d374668da4274363d2
César Gallego Rodríguez <gallego.cesar@gmail.com>
2019-02-22 15:15:37 +0100

    flow ussing session id

--- flow/flow.py
+++ flow/flow.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
-from itertools import tee
-from typing import Iterable
-from functools import partial
+from itertools import tee, groupby, chain
+from typing import Iterable, Optional, Tuple
+from functools import partial, reduce
 import json
 
 from sqlalchemy import Column, ForeignKey, Integer, String, BigInteger, Text
@@ -12,9 +12,10 @@ from sqlalchemy import create_engine
 Base = declarative_base()
 
 
-class Log(Base):
+class Log(Base):# type: ignore
     __tablename__ = 'proxy_logs'
     id = Column(Integer, primary_key=True, autoincrement=True)
+    proxy_session_id = Column(Text, nullable=False)
     request = Column(Text, nullable=False)
     response = Column(Text, nullable=False)
 
@@ -22,14 +23,16 @@ class Log(Base):
 @dataclass
 class ReqRes:
     key :int
+    session_id :str
     request :dict
     response :dict
 
 
-def parse_to_dataclass(log :Log) -> ReqRes:
+def parse_to_dataclass(log :Log) -> Optional[ReqRes]:
     try:
         return ReqRes(
             key=log.id,
+            session_id=log.proxy_session_id,
             request=json.loads(log.request),
             response=json.loads(log.response)
         )
@@ -39,17 +42,21 @@ def parse_to_dataclass(log :Log) -> ReqRes:
             print(exc)
             print(log.request)
             print(log.response)
+    return None
 
 
-def content_type(headers :dict) -> str:
+def content_type(headers :dict) -> Optional[str]:
     if "Content-Type" in headers:
         return headers["Content-Type"]
     if "content-type" in headers:
         return headers["content-type"]
+    return None
 
 def response_content_type_filter(expected :str, reqres :ReqRes) -> bool:
     content = content_type(reqres.response["headers"])
-    return expected in content
+    if content:
+        return expected in content
+    return False
 
 
 @dataclass
@@ -58,20 +65,33 @@ class Step:
     destination :str
 
 
-def content_type_flow(content_type :str, logs :Iterable[ReqRes])->Iterable[Step]:
-    only_html = partial(response_content_type_filter, content_type)
-    just_path = map(lambda x: x.request["path"], filter(only_html, logs))
-    
-    origin, destination = tee(just_path)
-    next(destination)
+def content_type_flow(
+        content_type :str, 
+        logs :Iterable[Tuple[str, Iterable[ReqRes]]]
+    ) -> Iterable[Step]:
+    target_content_filter = partial(response_content_type_filter, content_type)
+    def _by_time(elm :ReqRes) -> int:
+        return elm.request["timestamp_start"]
 
-    origin_destination = zip(origin, destination)
-    return map(lambda x: Step(x[0], x[1]), origin_destination)
+    def _proc_by_session(sess_log :Tuple[str, Iterable[ReqRes]]):
+        session, log = sess_log
+        target_content = list(filter(target_content_filter, log))
+        target_content.sort(key=_by_time)
+        just_path = map(lambda x: x.request["path"], target_content)
+        origin, destination = tee(just_path)
+        next(destination)
+
+        origin_destination = zip(origin, destination)
+        return map(lambda x: Step(x[0], x[1]), origin_destination)
+
+    by_session = map(_proc_by_session, logs)
+    return reduce(chain, by_session, iter([]))
+    
 
 
 def main():
-    def _sort_by_time(elm :ReqRes) -> int:
-        return elm.request["timestamp_start"]
+    def _by_session(elm :ReqRes) -> str:
+        return elm.session_id
 
     engine = create_engine('sqlite:///apicheck.db')
     Base.metadata.create_all(engine)
@@ -80,13 +100,17 @@ def main():
     session = DBSession()
 
     res = list(map(parse_to_dataclass, session.query(Log).all()))
-    res.sort(key=_sort_by_time)
+    res.sort(key=_by_session)
+    grouped_sessions :Tuple[str, Iterable[ReqRes]] = groupby(res, key=_by_session)
 
     processes = [
-        partial(content_type_flow, "html")
+        partial(content_type_flow, "html"),
+        partial(content_type_flow, "json")
     ]
-    analysis = map(lambda x: x(res), processes)
-    print(list(analysis))
+
+    analysis = map(lambda x: x[0](x[1]), zip(processes, tee(grouped_sessions)))
+    for x in analysis:
+        print(list(x))
 
 
 if __name__ == "__main__":
