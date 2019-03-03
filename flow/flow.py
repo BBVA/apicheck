90c4c1e4e212c46f7a3aad84d34bf81ed4505f4e
César Gallego Rodríguez <gallego.cesar@gmail.com>
2019-03-03 19:54:10 +0100

    some sanity refactor of flow

--- flow/flow.py
+++ flow/flow.py
@@ -1,6 +1,6 @@
-from dataclasses import dataclass
+import dataclasses
 from itertools import tee, groupby, chain
-from typing import Iterable, Optional, Tuple, Callable, Set
+from typing import Iterable, Optional, Tuple, Callable, Set, List
 from functools import partial, reduce
 import json
 import sys
@@ -21,7 +21,14 @@ class Log(Base):  # type: ignore
     response = Column(Text, nullable=False)
 
 
-@dataclass
+class DataClassJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+
+@dataclasses.dataclass
 class ReqRes:
     key: int
     session_id: str
@@ -46,14 +53,14 @@ def parse_to_dataclass(log: Log) -> Optional[ReqRes]:
     return None
 
 
-@dataclass
+@dataclasses.dataclass
 class RequestInfo:
     key: str
     response_time: float
     size: int
 
 
-@dataclass
+@dataclasses.dataclass
 class RequestStats:
     key: str = ""
     count: int = 0
@@ -65,7 +72,7 @@ class RequestStats:
     max_size: int = -sys.maxsize - 1
 
 
-def collect_request_info(requests: Iterable[ReqRes]) -> None:
+def collect_request_info(requests: List[ReqRes]) -> None:
     def _map_to_RequestInfo(reqres: ReqRes) -> RequestInfo:
         try:
             size = int(reqres.response["headers"]["Content-Length"])
@@ -98,14 +105,14 @@ def collect_request_info(requests: Iterable[ReqRes]) -> None:
         stats.min_size = min(stats.min_size, info.size)
         stats.max_size = max(stats.max_size, info.size)
         return stats
-
-    request_info = list(map(_map_to_RequestInfo, requests))
+    
+    request_info = [_map_to_RequestInfo(x) for x in requests]
     request_info.sort(key=_by_key)
     by_endpoint = groupby(request_info, key=_by_key)
-    return map(_collect_stats, by_endpoint)
+    return [_collect_stats(x) for x in by_endpoint]
 
 
-@dataclass
+@dataclasses.dataclass
 class Step:
     session: str
     origin: str
@@ -129,7 +136,7 @@ def response_content_type_filter(expected: str, reqres: ReqRes) -> bool:
 
 def content_type_flow(
         content_type: str,
-        logs: Iterable[Tuple[str, Iterable[ReqRes]]]
+        logs: List[Tuple[str, List[ReqRes]]]
 ) -> Iterable[Step]:
     target_content_filter = partial(response_content_type_filter, content_type)
 
@@ -137,18 +144,18 @@ def content_type_flow(
         return elm.request["timestamp_start"]
 
     def _proc_by_session(sess_log: Tuple[str, Iterable[ReqRes]]):
-        session, log = sess_log
-        target_content = list(filter(target_content_filter, log))
-        target_content.sort(key=_by_time)
-        just_path = map(lambda x: x.request["path"], target_content)
-        origin, destination = tee(just_path)
-        next(destination)
+        session_id, log = sess_log
+        log.sort(key=_by_time)
+        just_path = [
+            x.request["path"]
+            for x in log
+            if target_content_filter(x)
+        ]
 
-        origin_destination = zip(origin, destination)
-        return map(lambda x: Step(session, x[0], x[1]), origin_destination)
+        return [Step(session_id, origin, dest) for origin, dest in zip(just_path, just_path[1:])]
 
-    by_session = map(_proc_by_session, logs)
-    return reduce(chain, by_session, iter([]))
+    by_session = [_proc_by_session(x) for x in logs]
+    return list(reduce(chain, by_session, iter([])))
 
 
 # not in use
@@ -171,33 +178,33 @@ def main():
     DBSession = sessionmaker(bind=engine)
     session = DBSession()
 
-    res = list(map(parse_to_dataclass, session.query(Log).all()))
+    res = [parse_to_dataclass(x) for x in session.query(Log).all()]
 
+    info = {}
     # response metrics
     metrics = list(collect_request_info(res))
+    info["metrics"] = metrics
 
-    # flows
-    def _execute(x: (Callable, Iterable[ReqRes])) -> Iterable[Step]:
-        fun, reqres = x
-        return fun(reqres)
-
+    # Flows
     res.sort(key=_by_session)
     grouped_sessions = groupby(res, key=_by_session)
-    processes = [
-        partial(content_type_flow, "html"),
-        partial(content_type_flow, "json")
-    ]
+    just_sessions = [(x, list(s)) for x, s in grouped_sessions]
+    steps = {
+        "html": content_type_flow("html", just_sessions),
+        "json": content_type_flow("json", just_sessions)
+    }
+    info["steps"] = steps
 
-    steps = map(_execute, zip(processes, tee(grouped_sessions)))
-
-    # resources by host
+    # Resources by host
     def _by_host(elm: ReqRes) -> str:
         return elm.request["host"]
 
     res.sort(key=_by_host)
     by_host = groupby(res, key=_by_host)
-    only_path = map(lambda x: (x[0], [y.request["path"] for y in x[1]]), by_host)
-    print(list(map(lambda x: (x[0], list(x[1])), only_path)))
+    resources = [(host, [x.request["path"] for x in reqres]) for host, reqres in by_host]
+    info["resources"] = resources
+    with open("data.json", "w") as out:
+        json.dump(info, out, cls=DataClassJSONEncoder)
 
 
 if __name__ == "__main__":
