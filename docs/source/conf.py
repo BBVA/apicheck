85900f02f39053104583c7b282517039a44058cb
cr0hn <cr0hn@cr0hn.com>
2019-04-12 15:05:32 +0200

    fixed master_doc indexes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+master_doc = "index.rst"
 
 # -- Options for HTML output -------------------------------------------------
 
