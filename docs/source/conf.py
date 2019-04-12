e73095426e8a2ce3c33b93bacd85ed829a6604db
cr0hn <cr0hn@cr0hn.com>
2019-04-12 15:08:11 +0200

    fixed extension for doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,7 +41,7 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-master_doc = "index.rst"
+master_doc = "index"
 
 # -- Options for HTML output -------------------------------------------------
 
