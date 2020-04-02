cbcc651166118712d33e2721efc3ed6dd55df7c2
cr0hn <cr0hn@cr0hn.com>
2020-04-02 11:36:25 +0200

    add - docker build images

---
+++ build_docker_images.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+
+for tool_dir in $(ls tools); do
+
+  # Add plugin title to documentation file
+  DOCKERFILE="tools/${tool_dir}/Dockerfile"
+  DOCKER_IMAGE_NAME=$(cat tools/${tool_dir}/META | jq -r ".name")
+  DOCKER_IMAGE_VERSION=$(cat tools/${tool_dir}/META | jq -r ".version")
+
+  docker build -T ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} -t ${DOCKER_IMAGE_NAME}:latest ${DOCKERFILE}
+  docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
+  docker push ${DOCKER_IMAGE_NAME}:latest
