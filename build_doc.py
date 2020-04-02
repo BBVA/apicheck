2be496d3e8a5f680e4ce49488c7edffebd2d1255
cr0hn <cr0hn@cr0hn.com>
2020-04-02 16:15:03 +0200

    updated - doc generation now in Python

---
+++ build_doc.py
@@ -0,0 +1,64 @@
+"""
+This file build the documentation for APICheck
+"""
+import os
+import json
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+DOC_PATH = os.path.join(HERE, "docs")
+TOOLS_PATH = os.path.join(HERE, "tools")
+STATIC_PATH = os.path.join(HERE, "docs", "static")
+
+
+def main():
+
+    catalog = []
+
+    #
+    # Getting README from plugin
+    #
+    for d in os.listdir(TOOLS_PATH):
+
+        # Get README.md file
+        tools_path = os.path.join(TOOLS_PATH, d)
+
+        meta_path = os.path.join(tools_path, "META")
+        readme_path = os.path.join(tools_path, "README.md")
+
+        try:
+            with open(readme_path, "r") as readme_handler:
+                readme_text = readme_handler.readlines()
+        except OSError:
+            print(f"[!] Tool \"{d}\" doesnt has README.md file")
+            continue
+
+        try:
+            with open(meta_path, "r") as meta_handler:
+                catalog.append(json.load(meta_handler))
+        except OSError:
+            print(f"[!] Tool \"{d}\" doesnt has README.md file")
+            continue
+
+        #
+        # Build tools documentation
+        #
+        doc_tool_path = os.path.join(DOC_PATH, "content", d.replace("_", "-"))
+        readme_title = readme_text[0].replace("#", "").strip()
+
+        if not os.path.exists(doc_tool_path):
+            os.mkdir(doc_tool_path)
+
+        with open(os.path.join(doc_tool_path, "index.md"), "w") as f:
+            f.write(f"---\ntitle: {readme_title}\n---\n")
+            f.write("\n".join(readme_text))
+            f.flush()
+
+    #
+    # Build catalog
+    #
+    with open(os.path.join(STATIC_PATH, "catalog.json"), "w") as f:
+        json.dump(catalog)
+
+
+if __name__ == '__main__':
+    main()
--- build_docker_images.sh
+++ build_docker_images.sh
@@ -9,7 +9,7 @@ for tool_dir in $(ls tools); do
   DOCKER_IMAGE_NAME=$(cat tools/${tool_dir}/META | jq -r ".name")
   DOCKER_IMAGE_VERSION=$(cat tools/${tool_dir}/META | jq -r ".version")
 
-  docker build -T ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} -t ${DOCKER_IMAGE_NAME}:latest ${DOCKERFILE}
+  docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} -t ${DOCKER_IMAGE_NAME}:latest ${DOCKERFILE}
   docker push ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}
   docker push ${DOCKER_IMAGE_NAME}:latest
 done
