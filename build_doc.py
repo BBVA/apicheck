b157b0ac5a8fa689cf82477535f797280b3487d2
cr0hn <cr0hn@cr0hn.com>
2020-04-03 13:58:18 +0200

    updated - META in tools updated - build_doc.py to adapt to new format updated - renamed tools to avoid prefixing "ac-..." fix - sensitive-json. Removing setup.py installations

--- build_doc.py
+++ build_doc.py
@@ -11,10 +11,15 @@ DOC_PATH = os.path.join(HERE, "docs")
 TOOLS_PATH = os.path.join(HERE, "tools")
 STATIC_PATH = os.path.join(HERE, "docs", "static")
 
+META_KEYS = ("name", "short-command", "version", "description",
+             "home", "author")
+
 
 def main():
 
     catalog = []
+    short_commands = set()
+    tool_names = set()
 
     #
     # Getting README from plugin
@@ -39,7 +44,35 @@ def main():
                 cf = configparser.ConfigParser()
                 cf.read_string(f"[DEFAULT]\n {meta_handler.read()}")
 
-                catalog.append(dict(cf["DEFAULT"]))
+                meta = dict(cf["DEFAULT"])
+
+                # Check that META contains all needed keys
+                if not all(x in meta.keys() for x in META_KEYS):
+                    print(f"[!] Missing keys in META \"{d}\". "
+                          f"Needed keys: \"{', '.join(META_KEYS)}\"")
+                    exit(1)
+
+                #
+                # Check that 'name' and 'short-command' are unique
+                #
+                name = meta["name"]
+                short_command = meta["short-command"]
+
+                if short_command in short_commands:
+                    print(f"[!] Short-command \"{short_command}\" at tool "
+                          f"'{name}' already exits in another tool")
+                    exit(1)
+                else:
+                    short_commands.add(short_command)
+
+                if name in tool_names:
+                    print(f"[!] Tool name '{name}' already exits used "
+                          f"for other tool")
+                    exit(1)
+                else:
+                    tool_names.add(name)
+
+                catalog.append(meta)
 
         except OSError:
             print(f"[!] Tool \"{d}\" doesnt has README.md file")
