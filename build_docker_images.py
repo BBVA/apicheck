a2e3e5678f1703f98686da71e3428883445519a9
cr0hn <cr0hn@cr0hn.com>
2020-04-06 09:46:31 +0200

    imp - how to generate docker images add - install and info for the apickeck manager,

---
+++ build_docker_images.py
@@ -0,0 +1,134 @@
+"""
+This file build the docker images
+"""
+import os
+import sys
+import uuid
+import configparser
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+TOOLS_PATH = os.path.join(HERE, "tools")
+
+META_KEYS = ("name", "short-command", "version", "description",
+             "home", "author")
+
+DOCKER_HUB_REPO = "bbvalabs"
+
+
+def main():
+    docker_images = dict()
+
+    #
+    # Getting README from plugin
+    #
+    for d in os.listdir(TOOLS_PATH):
+
+        # Get META file
+        meta_path = os.path.join(TOOLS_PATH, d, "META")
+
+        try:
+            with open(meta_path, "r") as meta_handler:
+                cf = configparser.ConfigParser()
+                cf.read_string(f"[DEFAULT]\n {meta_handler.read()}")
+
+                meta = dict(cf["DEFAULT"])
+
+                # Check that META contains all needed keys
+                if not all(x in meta.keys() for x in META_KEYS):
+                    print(f"[!] Missing keys in META \"{d}\". "
+                          f"Needed keys: \"{', '.join(META_KEYS)}\"",
+                          file=sys.stderr)
+                    exit(1)
+
+                #
+                # Check that 'name' and 'short-command' are unique
+                #
+                name = meta["name"]
+                version = meta["version"]
+                docker_file_path = os.path.join(
+                    TOOLS_PATH,
+                    d,
+                    meta.get("docker-file", "Dockerfile"))
+
+                if not os.path.exists(docker_file_path):
+                    print(f"[!] Can't Dockerfile for tool '{name}'",
+                          file=sys.stderr)
+                    continue
+
+                if name in docker_images.keys():
+                    print(f"[!] Tool name '{name}' already exits used "
+                          f"for other tool",
+                          file=sys.stderr)
+                    exit(1)
+                else:
+
+                    docker_images[name] = (
+                        version,
+                        os.path.join(TOOLS_PATH, d),
+                        meta.get("docker-file", "Dockerfile")
+                    )
+
+        except OSError:
+            print(f"[!] Tool \"{d}\" doesnt has README.md file",
+                  file=sys.stderr)
+            continue
+
+    try:
+        docker_user = os.environ["DOCKER_USERNAME"]
+    except KeyError:
+        print("[!] Environment var 'DOCKER_USERNAME' needed",
+              file=sys.stderr)
+        exit(1)
+
+    try:
+        docker_password = os.environ["DOCKER_PASSWORD"]
+    except KeyError:
+        print("[!] Environment var 'DOCKER_PASSWORD' needed",
+              file=sys.stderr)
+        exit(1)
+
+    # Dump password to temporal file
+    password_file = f"/tmp/{uuid.uuid4().hex}"
+    with open(password_file, "w") as f:
+        f.write(docker_password)
+
+    #
+    # Build Docker commands
+    #
+    commands = []
+
+    # Do login
+    commands.append(" ".join([
+        f"cat {password_file}",
+        "|",
+        f"docker login --username {docker_user} --password-stdin"
+    ]))
+
+    # Add build command for each image
+    for image, (version, docker_file_path, docker_file) in docker_images.items():
+
+        # Go to the tool home for each building
+        commands.append(f"cd {docker_file_path}")
+
+        # Docker command
+        commands.append(" ".join([
+            f"docker build",
+            f" -t {DOCKER_HUB_REPO}/{image}:{version}",
+            f" -t {DOCKER_HUB_REPO}/{image}:latest",
+            f" -"
+            f" <"
+            f" {docker_file}"
+        ]))
+
+        # Go script base path
+        commands.append(f"cd {HERE}")
+
+    # Remove password file
+    commands.append(f"rm -rf {password_file}")
+
+    print("\n".join(commands))
+
+
+if __name__ == '__main__':
+    main()
--- tools/package-manager/package_manager/__main__.py
+++ tools/package-manager/package_manager/__main__.py
@@ -31,10 +31,28 @@ def add_new_alias(rc_file: str, alias: str):
     if not alias.startswith("alias"):
         alias = f"alias {alias}"
 
-    bashrc_path = Path.home().joinpath(rc_file)
+    bashrc_path = Path(rc_file)
     bashrc_text = bashrc_path.read_text()
     if alias not in bashrc_text:
-        bashrc_path.write_text(f'{bashrc_text}\n{alias}\n')
+
+        if not bashrc_text.endswith("\n"):
+            bashrc_text += "\n"
+
+        bashrc_path.write_text(f'{bashrc_text}{alias}\n')
+
+
+def rm_new_alias(rc_file: str, alias: str):
+    if not alias.startswith("unalias"):
+        alias = f"unalias {alias}"
+
+    bashrc_path = Path(rc_file)
+    bashrc_text = bashrc_path.read_text()
+    if alias not in bashrc_text:
+
+        if not bashrc_text.endswith("\n"):
+            bashrc_text += "\n"
+
+        bashrc_path.write_text(f'{bashrc_text}{alias}\n')
 
 
 def create_alias():
@@ -57,10 +75,19 @@ def get_catalog() -> List[dict]:
     return catalog_json
 
 
+def search_in_catalog(catalog: List[dict], tool_name: str) -> dict:
+    # Find tool
+    tool = None
+    for cat in catalog:
+        if cat["name"] == tool_name:
+            return cat
+    else:
+        return {}
+
+
 def print_table(content: List[Tuple[str, str]],
                 head: tuple = None,
                 width: int = 60):
-
     def border():
         # print("+", "-" * width, "+")
         print(f"+{'-' * width}+")
@@ -74,7 +101,9 @@ def print_table(content: List[Tuple[str, str]],
             print(f"{' ' * (max_key_len - 4)}", end="")
             print(" | ", end="")
             print(f"{head[1]}", end="")
-            print(f"{' ' * (width - len(head[0]) - len(head[1]) - max_key_len - 1)}", end="")
+            print(
+                f"{' ' * (width - len(head[0]) - len(head[1]) - max_key_len - 1)}",
+                end="")
             print(" |")
         else:
             print(f"| {head[0]}{' ' * (width - len(head[0]) - 1)}|")
@@ -120,7 +149,10 @@ def print_table(content: List[Tuple[str, str]],
                     print(" |")
                     first = False
                 else:
-                    print(f"| {' ' * max_key_len} | {text}{' ' * (column_size - len(text))}", end="")
+                    print(f"| "
+                          f"{' ' * max_key_len}"
+                          f" | "
+                          f"{text}{' ' * (column_size - len(text))}", end="")
                     print(" |")
 
                 if prev >= split_text_len:
@@ -136,19 +168,114 @@ def list_packages(args: argparse.Namespace):
                 content=[(x["name"], x["version"]) for x in catalog])
 
 
-def info_package(args: argparse.Namespace):
+def install_package(args: argparse.Namespace):
 
+    def build_alias_cmd(cmd: str, docker_image: str):
+        return f'''alias {cmd}="docker run --rm -it {docker_image}"'''
+
+    tool_name = args.tool_name
+    env_name = args.env_name or "default"
+    docker_image_name = tool_name.replace('_', '-')
+
+    catalog = get_catalog()
+
+    # Find tool
+    if not (tool := search_in_catalog(catalog, tool_name)):
+        print(f"[!] Can't find tool named '{tool_name}'")
+        exit(1)
+
+    catalog_tool_name = tool["name"]
+    catalog_short_command = tool["short-command"]
+
+    #
+    # Pull Docker image
+    #
+    print(f"[*] Fetching Docker image for tool '{tool_name}'")
+    docker_image_name = f"bbvalabs/{docker_image_name}"
+    docker_image_name = "python:3.8-alpine"
+
+    command = f"docker pull {docker_image_name}"
+
+    # os.system(command)
+
+    #
+    # Get / create environment config file
+    #
+    path = Path().home().joinpath(".apicheck_manager")
+
+    if not path.exists():
+        print(
+            f"[*] Creating path for storing apicheck environments at : "
+            f"{str(path)}"
+        )
+        os.mkdir(str(path))
+
+    env_path_route_deactivate = path.joinpath(f"{env_name}.deactivate")
+    deactivate_env_path = str(env_path_route_deactivate)
+    if not env_path_route_deactivate.exists():
+        print(f"[*] Creating deactivate env config for '{env_name}'")
+
+        header = [
+            f"""PS1=$(echo $PS1 | sed 's/[(]APICheck[)][ ]//g')""",
+        ]
+
+        with open(str(deactivate_env_path), "w") as f:
+            f.write("\n".join(header))
+
+    env_path_route = path.joinpath(env_name)
+    activate_env_path = str(env_path_route)
+    if not env_path_route.exists():
+        print(f"[*] Creating env config for '{env_name}'")
+
+        header = [
+            '''PS1="(APICheck) $PS1"''',
+            f'''alias deactivate="source {deactivate_env_path}"''',
+        ]
+
+        with open(str(activate_env_path), "w") as f:
+            f.write("\n".join(header))
+
+    #
+    # Fill env-file
+    #
+    print("[*] filling environment alias file ")
+
+    alias1 = build_alias_cmd(catalog_tool_name, docker_image_name)
+    alias2 = build_alias_cmd(catalog_short_command, docker_image_name)
+
+    add_new_alias(activate_env_path, alias1)
+    add_new_alias(activate_env_path, alias2)
+
+    rm_new_alias(deactivate_env_path, catalog_tool_name)
+    rm_new_alias(deactivate_env_path, catalog_tool_name)
+
+
+def activate_env(args: argparse.Namespace):
+    env_name = args.env_name or "default"
+
+    #
+    # Get / create environment config file
+    #
+    path = Path().home().joinpath(".apicheck_manager")
+
+    if not path.exists():
+        print("[!] Env name doesn't exits")
+        exit(1)
+
+    env_path_route = path.joinpath(env_name)
+    if not env_path_route.exists():
+        print("[!] Env name doesn't exits")
+
+    os.system(f"source {env_path_route}")
+
+
+def info_package(args: argparse.Namespace):
     tool_name = args.tool_name
 
     catalog = get_catalog()
 
     # Find tool
-    tool = None
-    for cat in catalog:
-        if cat["name"] == tool_name:
-            tool = cat
-            break
-    else:
+    if not (tool := search_in_catalog(catalog, tool_name)):
         print(f"[!] Can't find tool named '{tool_name}'")
         exit(1)
 
@@ -156,13 +283,15 @@ def info_package(args: argparse.Namespace):
     print_table(content=[
         tuple(y)
         for y in tool.items()
-    ], head=(f"Tool name '{tool_name}'", ))
+    ], head=(f"Tool name '{tool_name}'",))
 
 
 def main():
     actions = {
         "list": list_packages,
-        "info": info_package
+        "info": info_package,
+        "install": install_package,
+        "activate": activate_env
     }
 
     parser = argparse.ArgumentParser(description='APICheck Manager')
@@ -178,6 +307,28 @@ def main():
 
     tool_info = subparsers.add_parser('info', help='show expanded tool info')
     tool_info.add_argument("tool_name")
+
+    tool_info = subparsers.add_parser('install',
+                                      help='install an APICheck tool')
+    tool_info.add_argument("tool_name")
+    tool_info.add_argument("-e", "--environment-name",
+                           dest="env_name",
+                           default=None,
+                           help="custom environment name")
+
+    tool_activate = subparsers.add_parser('activate',
+                                          help='activate an environment')
+
+    tool_activate.add_argument(
+        "-e", "--environment-name",
+        dest="env_name",
+        default=None,
+        help="custom environment name")
+    tool_activate.add_argument(
+        "-H", "--docker-host",
+        dest="docker_host",
+        default=None,
+        help="docker url. default: tcp://127.0.0.1:2375")
 
     cli_parsed = parser.parse_args()
 
