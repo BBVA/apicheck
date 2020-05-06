5ca3ed5ea6087d12fdffaffc376d4cd6dd098a69
cr0hn <cr0hn@cr0hn.com>
2020-05-06 15:49:45 +0200

    updated - revision

--- tools/curl/setup.py
+++ tools/curl/setup.py
@@ -1,6 +1,13 @@
+from os import path
 from setuptools import setup, find_packages
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'requirements.txt')) as f:
+    required = f.read().splitlines()
 
 setup(
     name="gurl",
-    packages=find_packages()
+    packages=find_packages(),
+    install_requires=required
 )
