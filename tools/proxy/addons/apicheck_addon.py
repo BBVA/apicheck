bdaceb261fe155c1b60e4a0f421b46843aabbdd9
cr0hn <cr0hn@cr0hn.com>
2020-05-20 13:58:53 +0200

    updated - documentation with examples and configuration environmnet vars add - filter proxy interception by specific hostname fix - now proxy write at stdout APICheck format with intercepted request

--- tools/proxy/addons/apicheck_addon.py
+++ tools/proxy/addons/apicheck_addon.py
@@ -1,17 +1,66 @@
-from mitmproxy import http, ctx
+import json
+import base64
+
+from mitmproxy import http
 
 
-class Counter:
-    def __init__(self):
-        self.num = 0
+def to_apicheck_format(flow: http.HTTPFlow, is_error: bool = False) -> str:
 
-    def request(self, flow: http.HTTPFlow):
-        self.num = self.num + 1
-        ctx.log.info("XXXXXX")
-        ctx.log.error("XXXXXX")
+    _request = flow.request
+    _response = flow.response
 
+    data = {}
+
+    scheme = f"{_request.scheme}:" if _request.scheme else ""
+    url = f"{scheme}//{_request.host}:{_request.port}{_request.path or ''}"
+
+    if getattr(_request, "content", None):
+        request_content: bytes = _request.content
+    else:
+        request_content: bytes = _request.text.encode("UTF-8")
+
+    data["request"] = {
+        "url": url,
+        "method": _request.method,
+        "version": _request.http_version,
+        "headers": dict(_request.headers),
+        "body": base64.b64encode(request_content).decode("UTF-8"),
+    }
+
+    if _response:
+
+        if getattr(_request, "content", None):
+            response_content: bytes = _request.content
+        else:
+            response_content: bytes = _request.text.encode("UTF-8")
+
+        data["response"] = {
+            "status": _response.status_code,
+            "reason": _response.reason,
+            "headers": dict(_response.headers),
+            "body": base64.b64encode(response_content).decode("UTF-8"),
+        }
+
+    if is_error:
+        try:
+            meta: dict = data["_meta"]
+        except KeyError:
+            meta = {}
+            data["_meta"] = meta
+
+        meta["apicheck_proxy"] = {"error": "can't connect to server"}
+
+    return json.dumps(data)
+
+
+class APICheck:
+
+    def response(self, flow: http.HTTPFlow):
+        print(to_apicheck_format(flow, is_error=True), flush=True)
+
+    def error(self, flow: http.HTTPFlow):
+        print(to_apicheck_format(flow, is_error=True), flush=True)
 
 addons = [
-    Counter()
+    APICheck()
 ]
-
--- tools/proxy/apicheck_proxy.sh
+++ tools/proxy/apicheck_proxy.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-LISTEN_ADDR=${LISTEN_ADD:=0.0.0.0}
-LISTEN_PORT=${LISTEN_PORT:=8080}
+APICHECK_PROXY_LISTEN_ADDR=${APICHECK_PROXY_LISTEN_ADDR:=0.0.0.0}
+APICHECK_PROXY_LISTEN_PORT=${APICHECK_PROXY_LISTEN_PORT:=8080}
+APICHECK_PROXY_ALLOWED_HOST=${APICHECK_PROXY_ALLOWED_HOST:=*}
 
-mitmdump -s /addons/apicheck_addon.py --flow-detail 0 --cert *=/data/certificates/apicheck.pem --listen-host ${LISTEN_ADDR} --listen-port ${LISTEN_PORT}
+mitmdump -s /addons/apicheck_addon.py -q --flow-detail 0 --cert *=/data/certificates/apicheck.pem --listen-host ${APICHECK_PROXY_LISTEN_ADDR} --listen-port ${APICHECK_PROXY_LISTEN_PORT} --allow-hosts ${APICHECK_PROXY_ALLOWED_HOST}
 
